2b2f11f80165f38cb3d68cfddbe2d6afdc14702b
Rawan Sherif <45740176+rawannsherif@users.noreply.github.com>
2021-03-10 14:56:57 +0200

    Add files via upload

---
+++ lib/widget/users_info.dart
@@ -0,0 +1,61 @@
+import 'package:best_flutter_ui_templates/model/users.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart' as provider;
+
+import '../screens/edituser_screen.dart';
+import '../model/user.dart';
+
+class UserInfo extends StatelessWidget {
+  final String id;
+  final String fullname;
+  //final String imageUrl;
+
+  UserInfo(this.id, this.fullname);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(fullname),
+      /* leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
+      ),*/
+      trailing: Container(
+        width: 100,
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamed(EditUserScreen.routeName, arguments: id);
+              },
+              /*onPressed: () {
+
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => EditUserScreen(id),
+                    ));
+              },*/
+              color: Theme.of(context).primaryColor,
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () async {
+                try {
+                  provider.Provider.of<Users>(context, listen: false)
+                      .deleteUser(id);
+                } catch (error) {
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                    content: Text(error.toString()),
+                  ));
+                }
+              },
+              color: Theme.of(context).errorColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
